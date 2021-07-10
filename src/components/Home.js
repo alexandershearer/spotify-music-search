239d8d7e81a31e671e46654b0c3cc0e73ae708e2
alexandershearer <alex.shearer23@gmail.com>
2021-07-09 19:23:45 -0500

    Setup spotify login for user

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,11 +3,23 @@ import { connect } from 'react-redux'
 import { Button } from 'react-bootstrap'
 import Header from './Header'
 
-function Home(props) {
+
+const Home = (props) => {
+
+    const {
+        REACT_APP_CLIENT_ID,
+        REACT_APP_AUTHORIZE_URL,
+        REACT_APP_REDIRECT_URL
+    } = process.env;
+
+    const handleLogin = () => {
+        window.location = `${REACT_APP_AUTHORIZE_URL}?client_id=${REACT_APP_CLIENT_ID}&redirect_uri=${REACT_APP_REDIRECT_URL}&response_type=token&show_dialog=true`;
+    };
+
     return (
         <div className="login">
             <Header />
-            <Button varient="info" type="submit">
+            <Button varient="info" type="submit" onClick={handleLogin}>
                 Login to Spotify
             </Button>
         </div>
